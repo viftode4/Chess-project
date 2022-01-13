0912e9666083cdbafe938248a20687883a9a9f85
levladutz <viftode4@yahoo.com>
2022-01-13 20:57:53 +0200

    main menu stuff and some node

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/play", function(req, res) {
+    res.sendFile("game.html", { root: "./public" });
 });
-
+router.get("/splash", function(req, res) {
+    res.sendFile("splash.html", { root: "./public" });
+});
+router.get("/", function(req, res) {
+    res.sendFile("splash.html", { root: "./public" });
+});
 module.exports = router;
