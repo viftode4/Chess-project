1e437b87e670a08fd0b138f0a82447f6f12daa20
levladutz <viftode4@yahoo.com>
2022-01-13 21:38:57 +0200

    got routes working :)

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get("/play", function(req, res) {
-    res.sendFile("game.html", { root: "./public" });
-});
-router.get("/splash", function(req, res) {
-    res.sendFile("splash.html", { root: "./public" });
-});
 router.get("/", function(req, res) {
     res.sendFile("splash.html", { root: "./public" });
+});
+// PLAY BUTTON PAGE
+router.get("/play", function(req, res) {
+    res.sendFile("game.html", { root: "./public" });
 });
 module.exports = router;
